d38b0bde8edc49582608a047db33074ead3df330
Ngoc Luong <ngoc.luong@eastagile.com>
2015-08-09 02:19:28 +0700

    [f] Update event status

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class EventsController < Api::V1::ApplicationController
+      before_filter :set_event, only: [:update]
+
       def index
         render json: @user.events.to_a, each_serializer: EventSerializer
       end
@@ -24,12 +26,25 @@ module Api
         end
       end
 
+      def update
+        if @event.update_attributes(create_bill_options)
+          render json: EventSerializer.new(event)
+        else
+          response_with_error(@event.errors.full_messages.join('. '), :unprocessable_entity)
+        end
+      end
+
+      def set_event
+        @event = Event.where(owner: @user, id: params[:id]).first
+        not_found_error unless @event
+      end
+
       def required_params
         %w(name start_time)
       end
 
       def create_event_options
-        params.permit(:name, :start_time)
+        params.permit(:name, :start_time, :status)
       end
     end
   end
