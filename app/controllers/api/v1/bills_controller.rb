ba93f76e72e1017a8938a6f29bbae2081794409b
Ngoc Luong <ngoc.luong@eastagile.com>
2015-08-09 00:39:57 +0700

    [f] User can edit the bill

--- app/controllers/api/v1/bills_controller.rb
+++ app/controllers/api/v1/bills_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class BillsController < Api::V1::ApplicationController
+      before_filter :set_bill, only: [:update]
+
       def create
         if has_params?(*required_params)
           if bill = Bill.create(create_bill_options)
@@ -9,6 +11,22 @@ module Api
             response_with_error(bill.errors.full_messages.join('. '), :unprocessable_entity)
           end
         end
+      end
+
+      def update
+        if has_params?(*required_params)
+          if @bill.update_attributes(create_bill_options)
+            render json: BillSerializer.new(@bill)
+          else
+            response_with_error(@bill.errors.full_messages.join('. '), :unprocessable_entity)
+          end
+        end
+      end
+
+      def set_bill
+        event_ids = Event.where(owner: @user).pluck(:id)
+        @bill = Bill.where(:event_id.in => event_ids, id: params[:id]).first
+        not_found_error unless @bill
       end
 
       def create_bill_options
